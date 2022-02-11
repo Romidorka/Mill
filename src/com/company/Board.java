6d52379d453abcc668e92a81a87358ff90d169c1
Romidorka <artemvasnev01@gmail.com>
2022-02-11 15:41:52 +0000

    Added colors for points and lines

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -6,7 +6,13 @@ package com.company;
  2(#)- Black player(2)
 */
 
+
+
 public class Board {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+
     byte board[] = new byte[24];
 
     Board(){
@@ -21,16 +27,21 @@ public class Board {
         }
     }
 
+    public void fill(int value){
+        for(int i=0;i<24;i++){
+            board[i]=(byte)value;
+        }
+    }
+
     private String[] get_board_with_chars(){
         String tmpBoard[] = new String[24];
         for(int i=0;i<24;i++){
             switch (board[i]) {
-                case 0 -> tmpBoard[i] = ".";
-                case 1 -> tmpBoard[i] = "@";
-                case 2 -> tmpBoard[i] = "#";
+                case 0 -> tmpBoard[i] = " ";
+                case 1 -> tmpBoard[i] = ANSI_GREEN + "●" + ANSI_RESET;
+                case 2 -> tmpBoard[i] = ANSI_RED + "●" + ANSI_RESET;
             }
         }
-
         return tmpBoard;
     }
 
@@ -38,13 +49,13 @@ public class Board {
         String strBoard[] = get_board_with_chars();
 
         System.out.printf("""
-                        %s     %s     %s
-                          %s   %s   %s
-                            %s %S %S
-                        %s %s %s   %s %s %s
-                            %s %S %S
-                          %s   %s   %s
-                        %s     %s     %s%n""",  strBoard[0], strBoard[1], strBoard[2], strBoard[3], strBoard[4], strBoard[5],
+                        %s⎻⎻⎻⎻⎻%s⎻⎻⎻⎻⎻%s
+                        | %s⎻⎻⎻%s⎻⎻⎻%s |
+                        | | %s⎻%s⎻%s | |
+                        %s⎻%s⎻%s   %s⎻%s⎻%s
+                        | | %s⎻%s⎻%s | |
+                        | %s⎻⎻⎻%s⎻⎻⎻%s |
+                        %s⎻⎻⎻⎻⎻%s⎻⎻⎻⎻⎻%s%n""",  strBoard[0], strBoard[1], strBoard[2], strBoard[3], strBoard[4], strBoard[5],
                                 strBoard[6], strBoard[7], strBoard[8], strBoard[9], strBoard[10], strBoard[11],
                                 strBoard[12], strBoard[13], strBoard[14], strBoard[15], strBoard[16], strBoard[17],
                                 strBoard[18], strBoard[19], strBoard[20], strBoard[21], strBoard[22], strBoard[23]);
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
         Board board = new Board();
-        board.randomize_board();
+//        board.randomize_board();
+        board.fill(1);
         System.out.println(Arrays.toString(board.board));
         board.print_board();
     }
